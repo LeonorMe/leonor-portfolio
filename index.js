345be2813be7f8776695d19a98cb5127ea2d5ebb
Leonor <53794672+LeonorMe@users.noreply.github.com>
2024-12-06 14:40:01 +0100

    water color changes with color mode

---
+++ index.js
@@ -0,0 +1,5 @@
+/* let waterColorEle = document.querySelector(".water-color");
+let r = document.querySelector(":root");
+waterColorEle.fill = getComputedStyle(r).getPropertyValue("--lake");
+console.log("ind ", waterColorEle.fill);
+*/
