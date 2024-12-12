039a93010a03754ee35686e4905f6fabd942336f
Leonor <53794672+LeonorMe@users.noreply.github.com>
2024-12-12 20:16:01 +0100

    testing manually create nenufar groups

--- svgLoadObjects.js
+++ svgLoadObjects.js
@@ -7,5 +7,21 @@ document.querySelectorAll(".fish-blur").forEach((fishBlur) => {
 });
 
 document.querySelectorAll(".water").forEach((water) => {
-    water.innerHTML = svgWater
+    water.innerHTML = svgWater;
 })
+
+document.querySelectorAll(".main").forEach((main) => {
+    main.innerHTML = svgMain;
+})
+
+document.querySelectorAll(".flower").forEach((flower) => {
+    flower.innerHTML = svgFlower;
+});
+
+document.querySelectorAll(".nenufar-big").forEach((e) => {
+    e.innerHTML = svgNenufarBig;
+});
+
+document.querySelectorAll(".nenufar-small").forEach((e) => {
+    e.innerHTML = svgNenufarSmall;
+});
