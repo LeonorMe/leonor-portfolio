8d8cd73f39e077645ffe3bf1741bc75c853a5994
Leonor <53794672+LeonorMe@users.noreply.github.com>
2024-12-18 03:38:34 +0100

    about pop-up base done

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let r = document.querySelector(":root");
 waterColorEle.fill = getComputedStyle(r).getPropertyValue("--lake");
 console.log("ind ", waterColorEle.fill);
 */
+
+function openAbout() {
+    document.querySelector(".about").style.display = "block";
+}
+
+function closeAbout() {
+    document.querySelector(".about").style.display = "none";
+}
