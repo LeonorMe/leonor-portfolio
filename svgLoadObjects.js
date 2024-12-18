1b8f4a9b095a1a24f6888fc378ace5223a58bab6
Leonor <53794672+LeonorMe@users.noreply.github.com>
2024-12-18 02:18:54 +0100

    index header ready

--- svgLoadObjects.js
+++ svgLoadObjects.js
@@ -72,7 +72,8 @@ document.querySelectorAll(".PT-pt").forEach((e) => {
         <div class="profile"></div>
         <div class="nenufar-big"></div>
         <div class="nenufar-small"></div>
-        <h1>Leonor <br> Medeiros <br><br> Portfolio</h1>
+        <h1>Leonor <br> Medeiros</h1>
+        <p class="subtitle">Portfolio</p>
     </div>
 */
 
@@ -84,5 +85,21 @@ document.querySelectorAll(".PT-pt").forEach((e) => {
         <div class="EN-en"></div>
         <div class="PT-pt"></div>
         <p>@LM <br> dec 2024</p>
+    </div>
+*/
+
+/* CONTACT
+    <div class="group-contact">
+        <div class="flower"></div>
+        <a class="nenufar-big" href="/contact.html"></a>
+        <div class="nenufar-small github"></div>
+        <div class="nenufar-small linkedin"></div>
+        <div class="nenufar-small artstation"></div>
+        <div class="nenufar-small youtube"></div>
+        <div class="title"><h2>Contact Me</h2></div>
+        <div class="github-link"><a target="_blank" href="https://github.com/LeonorMe">GitHub</a></div>
+        <div class="linkedin-link"><a target="_blank" href="https://www.linkedin.com/in/leonor-medeiros-ab9269234/">LinkedIn</a></div>
+        <div class="artstation-link"><a target="_blank" href="https://leonormedeiros.artstation.com/">Art Station</a></div>
+        <div class="youtube-link"><a target="_blank" href="https://www.youtube.com/channel/UCfm2RQ1aN3jFYA5qJDPV7EQ">Youtube</a></div>
     </div>
 */
